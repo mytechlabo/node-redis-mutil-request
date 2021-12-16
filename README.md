2420b299dea6f5e9a8d6198449ca52e8000e6b7f
toan.trinh <toan.trinh@ntq-solution.com.vn>
2021-12-16 15:27:18 +0700

    init-project

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# node-redis-mutil-request
+# Node Redis Mutil Request
+
+## Setup
+### Install package
+```sh 
+$ npm install
+```
+
+```sh
+$ npm redis.v1.js
+$ npm redis.v2.js
+
+$ curl http://localhost:3000/order
+```
+
+
+## How to subscribe to multiple channels on Redis NodeJS
+
+```javascript
+var redis = require('redis')
+
+const subscribe = redis.createClient({
+    host: 'localhost',
+    port: 6379
+}) 
+subscribe.psubscribe(`user:chat:*`)
+
+subscribe.on('message', function(pattern, channel, message) {
+    console.log(channel, message, pattern)
+    // Write Your Awesome Code here.
+})
+```
+
+## Video
+- [Bài toán tồn kho còn 1 nhưng có nhiều USERS mua hàng cùng một lúc - P1](https://www.youtube.com/watch?v=qxRtMizGvWE)
+- [Bài toán tồn kho còn 1 nhưng có nhiều USERS mua hàng cùng một lúc - P2](https://www.youtube.com/watch?v=OUGfEleDGaE)
+
+https://stackoverflow.com/questions/66123057/redis-connection-with-promise
+https://stackoverflow.com/questions/70197001/unhandledpromiserejectionwarning-error-the-client-is-closed-in-nodejs-and-redi
+https://stackoverflow.com/questions/70160820/cannot-connect-to-redis-using-node-js-module
---
+++ redis.v2.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+
+const { get, set, setnx, incrby, exists } = require("./model.redis");
+
+app.get("/order", async (req, res) => {
+  const time = new Date().getTime();
+  console.log(`Time: `, time);
+
+  // Gia su so luong ton kho hien tai con 10
+  const slTonKho = 10;
+
+  // Ten cua san pham
+  const keyName = "iPhone13";
+
+  // Gia su moi lan mau thi luong tieu thu hang ton kho la 1
+  const slMua = 1;
+
+  // So luong da ban ra, neu chua ban thi set = 0, nguoc lai thi update + 1 moi lan mua thanh cong
+  const getKey = await exists(keyName);
+  if (!getKey) {
+    // set = 0
+    // await set(keyName, 0);
+
+    // Luu y: Neu co dong thoi tu 2 hay nhieu nguoi truy cap mua hang thi su dung setnx se khong duoc dat lai
+    await setnx(keyName, 0);
+  }
+
+  // Lay so luong ban ra
+  let slBanra = await get(keyName);
+  console.log("Truoc khi dat hang thanh cong, tong ban ra: ", slBanra);
+
+  // Neu so luong ban ra (slBanra) + so luong mua (slMua) > ton kho (slTonKho) return failed;
+
+  slBanra = await incrby(keyName, slMua); // Atom redis
+  if (slBanra > slTonKho) {
+    console.log("HET HANG!");
+    return res.json({
+      status: "error",
+      message: "order fail!",
+      time,
+    });
+  }
+
+  // Neu mua thanh cong
+  // slBanra = await incrby(keyName, slMua); // Atom redis
+  console.log("Sau khi dat hang thanh cong, tong ban ra: ", slBanra);
+  if (slBanra > slTonKho) {
+    await set("yeu cau khong hop le: ", slBanra - slTonKho);
+  }
+
+  return res.json({
+    status: "success",
+    message: "order success!",
+    time,
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`The server is running 3000`);
+});
